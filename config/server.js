57682ecc7707cb84d3881e6a4c2a569e5c6028a4
sak8767 <platformuser@kbitpro.com>
2021-10-04 01:24:34 -0400

    tettest

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '793c4faa230db8ca07f20ec738453de4'),
+    },
+  },
+});
